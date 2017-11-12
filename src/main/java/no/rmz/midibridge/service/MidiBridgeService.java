9f00e073fec90b2b2b2818305a9f06a8c73cafac
Bjørn Remseth <rmz@comoyo.com>
2017-11-12 01:23:28 +0100

    Actually use the command line

--- src/main/java/no/rmz/midibridge/service/MidiBridgeService.java
+++ src/main/java/no/rmz/midibridge/service/MidiBridgeService.java
@@ -9,9 +9,6 @@ import no.rmz.midibridge.config.MidibridgeConfiguration;
 public class MidiBridgeService extends Application<MidibridgeConfiguration> {
 
     public static void main(String[] args) throws Exception {
-        // XXX Setting args to make it simpler to debug, not what we should do when
-        //     building standalone jars.
-        args = new String[]{"server", "midibridge-config.yaml"};
         new MidiBridgeService().run(args);
     }
 
